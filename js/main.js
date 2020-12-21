ab7ea38882d7871db500bb6fa4a54f92ba8d0fd4
Sujin <kgj1212_@naver.com>
2020-12-21 15:53:30 +0900

    세번째 section 완료

--- js/main.js
+++ js/main.js
@@ -65,9 +65,16 @@
         messageB: document.querySelector('#scroll-section-2 .desc-message.b'),
         messageC: document.querySelector('#scroll-section-2 .desc-message.c'),
         pinB: document.querySelector('#scroll-section-2 .desc-message.b .pin'),
-        pinC: document.querySelector('#scroll-section-2 .desc-message.c .pin')
+        pinC: document.querySelector('#scroll-section-2 .desc-message.c .pin'),
+        canvas: document.querySelector('#video-canvas-1'),
+        context: document.querySelector('#video-canvas-1').getContext('2d'),
+        videoImages: []
       },
       values: {
+          videoImageCount: 960,
+          imageSequence:[0, 959],
+          canvas_opacticy_in:[0, 1, { start: 0, end: 0.1 }],
+          canvas_opacticy_out:[1, 0, { start: 0.95, end: 1 }],
           messageA_translateY_in: [20, 0, { start: 0.15, end: 0.2 }],
           messageB_translateY_in: [30, 0, { start: 0.6, end: 0.65 }],
           messageC_translateY_in: [30, 0, { start: 0.87, end: 0.92 }],
@@ -106,6 +113,13 @@
       imgElem.src=`./video/001/IMG_${6726 + i}.JPG`;
       sceneInfo[0].objs.videoImages.push(imgElem);
     }
+
+    let imgElem2;
+    for (let i = 0; i < sceneInfo[2].values.videoImageCount; i++) {
+      imgElem2 = new Image();
+      imgElem2.src=`./video/002/IMG_${7027 + i}.JPG`;
+      sceneInfo[2].objs.videoImages.push(imgElem2);
+    }
   }
   setCanvasImages();
 
@@ -136,6 +150,7 @@
 
     const heightRatio = window.innerHeight / 1080;
     sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
+    sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
   };
 
   function calcValues(values, currentYOffset) {
@@ -225,6 +240,18 @@
 
       case 2:
         // console.log('2 play')
+        let sequence2 = Math.round(calcValues(values.imageSequence, currentYOffset));
+        // console.log(sequence);
+        objs.context.drawImage(objs.videoImages[sequence2], 0, 0);
+
+        if (scrollRatio <= 0.5) {
+          // in
+          objs.canvas.style.opacity = calcValues(values.canvas_opacticy_in, currentYOffset);
+        } else {
+          // out
+          objs.canvas.style.opacity = calcValues(values.canvas_opacticy_out, currentYOffset);
+        }
+
         if (scrollRatio <= 0.32) {
             // in
             objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset);
