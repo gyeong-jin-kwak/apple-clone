73bce08b5f23a309fcb3a7cd7687cdf273000482
Sujin <kgj1212_@naver.com>
2021-01-08 14:24:39 +0900

    [feat] 브라우저 리사이즈 대응

--- js/main.js
+++ js/main.js
@@ -145,8 +145,7 @@
       sceneInfo[3].objs.images.push(imgElem3);
     }
     // console.log(sceneInfo[3].objs.images);
-  }
-  setCanvasImages();
+  };
 
   function checkMenu() {
     if(yOffset > 44){
@@ -561,5 +560,13 @@
     setLayout();
     sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
   });
-  window.addEventListener('resize', setLayout);
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 900) {
+      setLayout();
+    }
+    sceneInfo[3].values.rectStartY = 0;
+  });
+  window.addEventListener('orientationchange', setLayout);
+  
+  setCanvasImages();
 })();
